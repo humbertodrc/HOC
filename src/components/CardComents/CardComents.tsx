a913dc227b22986a405415e83a796eae48ebd9fa
danieldamian09 <humbertorivero9@gmail.com>
2022-10-27 21:44:12 -0300

    Add interface

--- src/components/CardComents/CardComents.tsx
+++ src/components/CardComents/CardComents.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Data } from '../../interface/data';
 import styles from './styles/CardComents.module.css';
 export interface CardComentsProps {
-	data?: any;
+	data?: Data[];
 }
 
 const CardComents : React.FC<CardComentsProps> = ({data}) => {
 	return <div className={styles.cardcoments}>
-		{data.map((item: any) => (
+		{data?.map((item: any) => (
 			<div key={item.id}>
 				<h3>{item.name}</h3>
 				<p>{item.body}</p>
@@ -14,5 +15,7 @@ const CardComents : React.FC<CardComentsProps> = ({data}) => {
 			))}
 	</div >;
 };
+
+
 
 export default CardComents;
--- src/components/CardPost/CardPost.tsx
+++ src/components/CardPost/CardPost.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Data } from '../../interface/data';
 import styles from './styles/CardPost.module.css';
 export interface CardPostProps {
-	data?: any;
+	data?: Data[];
 }
 
 const CardPost: React.FC<CardPostProps> = ({ data }) => {
 	
 	return <div className={styles.cardpost}>
-		{data.map((item: any) => (
+		{data?.map((item: any) => (
 			<div key={item.id}>
 				<h3>{item.title}</h3>
 				<p>{item.body}</p>
